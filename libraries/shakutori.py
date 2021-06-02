d08f48d05a838c906ae172a8a8498c8208e2dc25
yuuto_ohnuki <yuuto.0902@dna.bio.keio.ac.jp>
2021-06-02 13:02:28 +0900

    update shakurori.py

--- libraries/shakutori.py
+++ libraries/shakutori.py
@@ -17,3 +17,20 @@ while r<n:
         v[A[l]] = False
         l += 1
 print(ans)
+
+# 配列aの部分和がk以上になるものの個数 (ABC130-D)
+n,k = map(int, input().split())
+a = list(map(int, input().split()))
+ans, tmp = 0, 0
+right = 0
+for left in range(n):
+    while right < n:
+        if tmp < k:
+            tmp += a[right]
+            right += 1
+        else:
+            break
+    if tmp >= k:
+        ans += n - right + 1
+    tmp -= a[left]
+print(ans)
